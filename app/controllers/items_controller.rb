a70aaa701a99b3139e025b944fb2314cf8577cb8
rettofe <ferettosan@gmail.com>
2024-07-26 17:48:34 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      render :show
+      render :show,status: :unprocessable_entity
     end
   end
    def update
